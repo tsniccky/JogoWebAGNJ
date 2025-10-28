2630f8cff84ddb98664e20d2ab85462e936dcc9b
Augusto___ <augusto.tomazini@aluno.farroupilha.ifrs.edu.br>
2025-10-28 14:13:09 -0300

    Refactor YOUshallCHOOSE function for clarity

--- game/DialogueOptionManagement.js
+++ game/DialogueOptionManagement.js
@@ -10,7 +10,6 @@ function YOUshallCHOOSE(text, img, options, onComplete) {
   optionsContainer.innerHTML = '';
   optionsContainer.style.display = 'flex';
 
-  // Create options
   options.forEach((option) => {
     const optEl = document.createElement('p');
     optEl.className = 'option';
@@ -18,7 +17,6 @@ function YOUshallCHOOSE(text, img, options, onComplete) {
     optionsContainer.appendChild(optEl);
   });
 
-  // Highlight management
   let selectedIndex = 0;
   const optionElements = Array.from(optionsContainer.querySelectorAll('.option'));
   optionElements[selectedIndex].classList.add('highlighted');
@@ -39,23 +37,17 @@ function YOUshallCHOOSE(text, img, options, onComplete) {
     } else if (e.key === 'Enter') {
       const selectedOption = options[selectedIndex];
 
-      // Hide options
       optionsContainer.style.display = 'none';
       optionsContainer.innerHTML = '';
 
-      // Remove listener
       document.removeEventListener('keydown', onKeyDown);
 
       console.log("Option chosen:", selectedOption);
 
-      // Speak the choice
       if (selectedOption.callback) selectedOption.callback();
 
-      // Continue to next step
-      // Continue to next step
       if (onComplete) onComplete(PlayerChoice = selectedOption.value);
 
-      // Re-enable Enter for the next dialogue line
       if (window.resumeCutsceneInput) window.resumeCutsceneInput();
 
     }
@@ -64,3 +56,4 @@ function YOUshallCHOOSE(text, img, options, onComplete) {
   document.addEventListener('keydown', onKeyDown);
   if (window.pauseCutsceneInput) window.pauseCutsceneInput();
 }
+
